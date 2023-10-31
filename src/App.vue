12c5c4acafba79cd2a329662f864627e737c5d83
edinadepner11 <edinadepner@yahoo.com>
2023-10-31 18:44:12 +0100

    Remove unused code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div class="app">
-    hello
-  </div>
- </template>
+  <div class="app">hello</div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "App",
-  components: {}
+  components: {},
 });
 </script>
 
-<style>
-</style>
+<style></style>
