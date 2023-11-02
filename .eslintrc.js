4bca35077379e7ad42406e82b39236fdf2b733fb
edinadepner11 <edinadepner@yahoo.com>
2023-11-02 23:49:32 +0100

    Add EOL rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: [
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
+    // "eslint:recommended",
     "@vue/typescript/recommended",
     "plugin:prettier/recommended",
   ],
@@ -15,5 +15,11 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
 };
