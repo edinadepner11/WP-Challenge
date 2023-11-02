c1f853ca818e3672f652aa0377a1a90aece7b939
edinadepner11 <edinadepner@yahoo.com>
2023-11-02 23:58:11 +0100

    Generated by the routing CLI

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+</script>
+
+<template>
+  <div class="about">
+    <button @click="count++">Click me</button>
+    <h1>{{ count }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
