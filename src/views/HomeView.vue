1116efebb3892483708bebb96854ac567c72d08f
edinadepner11 <edinadepner@yahoo.com>
2023-11-05 23:51:41 +0100

    Import CategoriesList component and CategoriesDataSet

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="home">
-    <v-btn> Button </v-btn>
+    <CategoriesList :categories="categories" />
   </div>
 </template>
 
 <script lang="ts">
+import { defineComponent, ref } from "vue";
+// import CategoriesList from "./components/CategoriesList.vue";
+import Category from "../types/Categories";
+import { categoriesDataSet } from "../dataSets/Categories.dataset";
+import CategoriesList from "@/components/CategoriesList.vue";
+
 export default {
   name: "HomeView",
+  components: { CategoriesList },
+
+  setup() {
+    const categories = ref<Category[]>(categoriesDataSet);
+
+    return { categories };
+  },
 };
 </script>
