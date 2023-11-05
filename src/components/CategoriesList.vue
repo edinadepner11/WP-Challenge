32353087f4670dc939a2ced7ec81a56afedcdf97
edinadepner11 <edinadepner@yahoo.com>
2023-11-05 23:48:34 +0100

    Add logo column, actions column, and delete category action

--- src/components/CategoriesList.vue
+++ src/components/CategoriesList.vue
@@ -1,18 +1,35 @@
+<!-- eslint-disable vue/valid-v-slot -->
 <template>
-  <v-data-table
-    v-model:items-per-page="itemsPerPage"
-    :headers="tableHeaders"
-    :items="categoriesRow"
-    item-value="name"
-    class="elevation-1"
-  ></v-data-table>
+  <div class="outer-div">
+    <VDataTable
+      :key="categoryData"
+      v-model:items-per-page="itemsPerPage"
+      :headers="tableHeaders"
+      :items="categoriesRow"
+      item-value="name"
+      class="elevation-1 inner-table"
+      ><template v-slot:item.logo="{ item }">
+        <v-img :src="item.logo" alt="Category Icon" max-width="30px"></v-img>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <div>
+          <v-btn @click="onViewClick(item)">
+            <v-icon>mdi-eye</v-icon>
+          </v-btn>
+          <v-btn @click="onDeleteClick(item)">
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </div>
+      </template></VDataTable
+    >
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import Category from "@/types/Categories";
-import { VDataTable } from "vuetify/lib/labs/components.mjs";
-
+// import { VDataTable } from "vuetify/lib/labs/components.mjs";
+// import { VDataTable } from "vuetify/labs/VDataTable";
 export default defineComponent({
   data() {
     const filteredHeaders = [];
@@ -21,33 +38,47 @@ export default defineComponent({
         filteredHeaders.push(key);
       }
     }
+    console.log(this.categories.length);
     return {
+      categoryData: this.categories.length,
       itemsPerPage: 5,
       tableHeaders: [
         {
-          title: "Logo",
+          text: "Logo",
           value: "logo",
-          align: "center",
+          align: "end",
           sortable: true,
         },
         ...filteredHeaders.map((key) => {
           return {
             title: key,
             value: key,
-            align: "center",
+            align: "end",
             sortable: true,
           };
         }),
+        {
+          title: "Actions",
+          value: "actions",
+          align: "end",
+          sortable: false,
+        },
       ],
       categoriesRow: this.categories.map((category) => {
         return {
           logo: category.iconUrl,
           name: category.name,
           slug: category.slug,
-          activeFrom: category.activeFrom,
-          activeUntil: category.activeUntil,
-          createdAt: category.createdAt,
-          updatedAt: category.updatedAt,
+          activeFrom: category.activeFrom ? category.activeFrom : "N/A",
+          activeUntil: category.activeUntil ? category.activeUntil : "N/A",
+          createdAt: category.createdAt.slice(
+            0,
+            category.createdAt.indexOf("T")
+          ),
+          updatedAt: category.updatedAt.slice(
+            0,
+            category.createdAt.indexOf("T")
+          ),
         };
       }),
     };
@@ -59,14 +90,35 @@ export default defineComponent({
       type: Array as PropType<Category[]>,
     },
   },
+
+  methods: {
+    onViewClick(item: any) {
+      console.log(item);
+    },
+
+    onDeleteClick(item: any) {
+      const { slug } = item;
+      const index = this.categoriesRow.findIndex(
+        (category) => category.slug === slug
+      );
+
+      if (index !== -1) {
+        // eslint-disable-next-line vue/no-mutating-props
+        this.categoriesRow.splice(index, 1);
+      }
+    },
+  },
 });
 </script>
 
 <style scoped>
-.custom-div {
-  width: 90%;
+.outer-div {
+  width: 85%;
+  margin-left: 7.5%;
+  margin-right: 7.5%;
 }
-.icon {
-  width: 5%;
+.inner-table {
+  width: 100%;
+  border-radius: 10px;
 }
 </style>
